d0e186ff04e9db488de057cdd1203bdc33c91e25
Subhajit Sahu <wolfram77@gmail.com>
2019-04-17 13:34:50 +0530

    feature: list devices

---
+++ assets/dlist.js
@@ -0,0 +1,36 @@
+const $p = document.querySelector('p');
+const $table = document.querySelector('#table');
+const $sql = document.querySelector('#sql');
+const $tbody = document.querySelector('tbody');
+var options = {};
+
+
+
+function searchParse(search) {
+  var search = search.substring(1);
+  return search? JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"')
+  .replace(/&/g, '","').replace(/=/g,'":"') + '"}'):{};
+}
+
+function onReady() {
+  var o = searchParse(location.search);
+  console.log('onReady()', o);
+  return o;
+}
+
+async function request(o) {
+  var table = $table.value, data = {sql: $sql.value};
+  var ds = await m.request({method: 'GET', url: `/table/${table}.device`, data});
+  m.render($tbody, ds.map(d => m('tr', [
+    m('td', m('a', {href: `http://${d.address}`}, d.id)),
+    m('td', d.latitude), m('td', d.longitude),
+    m('td', d.location), m('td', d.city)])));
+}
+
+
+
+options = onReady();
+request(options);
+setInterval(() => request(options).then(() => {
+  m.render($p, null);
+}, err => m.render($p, err.message)), 1000);
